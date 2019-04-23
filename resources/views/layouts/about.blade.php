17b8849e0a441cc6026920f3e2b6ea47946de691
sarziv <saruzivila12@gmail.com>
2019-04-23 20:21:41 +0300

    Project closed.

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -2,26 +2,23 @@
     <div id="about" class="infoNameAbout brush">About</div>
     <div class="infoDataAbout">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-6 about_background ">
-                <img class="img-responsive"
-                     src="/images/6759506.jpg"
-                     style="max-height:100%;max-width: 100%;">
-            </div>
-            <div class="col-6">
+
                 <div class="about_info text-center">
                     <div class="about_font">I'm Šarunas Živila</div>
                     <br>
                     <p>Freelancing web developer.</p>
                     <p>Mostly working on back-end,with PHP frameworks.</p>
                     <p>I am based in Lithuania and have worked with clients<br> from all over the world.</p>
-                    <p></p>
-                    <p></p>
-                    <p>Also student at KTU for bachelor degree in computer science.</p>
-
+                    <p>Student at KTU for bachelor degree in computer science.</p>
+                    <div class="about_background ">
+                        <img class="img-responsive"
+                             src="/images/6759506.jpg"
+                             style="max-height:100%;max-width: 100%;">
+                    </div>
                 </div>
-            </div>
+
+
         </div>
-        </div>
+
     </div>
 </div>
